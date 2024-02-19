004bf4f7f5629aa2d28986e580941f196020bd89
Youhana Omi <74501876+youhana-omi@users.noreply.github.com>
2024-02-19 12:05:17 +0600

    DB added

---
+++ lms(1).sql
@@ -0,0 +1,276 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 19, 2024 at 06:57 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `lms`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admins`
+--
+
+CREATE TABLE `admins` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(250) NOT NULL,
+  `mobile` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `admins`
+--
+
+INSERT INTO `admins` (`id`, `name`, `email`, `password`, `mobile`) VALUES
+(1, 'admin', 'admin@gmail.com', '123', 1722222222);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `authors`
+--
+
+CREATE TABLE `authors` (
+  `author_id` int(11) NOT NULL,
+  `author_name` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `authors`
+--
+
+INSERT INTO `authors` (`author_id`, `author_name`) VALUES
+(101, 'Dennis Ritchie'),
+(102, 'Herbert Schildt'),
+(104, 'E. Balagurusamy');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `books`
+--
+
+CREATE TABLE `books` (
+  `book_id` int(11) NOT NULL,
+  `book_name` varchar(250) NOT NULL,
+  `author_id` int(11) NOT NULL,
+  `cat_id` int(11) NOT NULL,
+  `book_no` int(11) NOT NULL,
+  `book_price` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `books`
+--
+
+INSERT INTO `books` (`book_id`, `book_name`, `author_id`, `cat_id`, `book_no`, `book_price`) VALUES
+(2, 'Software engineering', 101, 1, 4518, 270),
+(3, 'Data structure', 102, 2, 6541, 300),
+(8, 'Programming in C', 101, 1, 20, 3000),
+(9, 'Data Mining', 105, 1, 50, 5000);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `category`
+--
+
+CREATE TABLE `category` (
+  `cat_id` int(11) NOT NULL,
+  `cat_name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `category`
+--
+
+INSERT INTO `category` (`cat_id`, `cat_name`) VALUES
+(1, 'Computer Science'),
+(2, 'Motivational'),
+(4, 'Historical'),
+(9, 'Novel');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `issued_books`
+--
+
+CREATE TABLE `issued_books` (
+  `s_no` int(11) NOT NULL,
+  `book_no` int(11) NOT NULL,
+  `book_name` varchar(200) NOT NULL,
+  `book_author` varchar(200) NOT NULL,
+  `student_id` int(11) NOT NULL,
+  `status` int(11) NOT NULL,
+  `issue_date` longtext NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `issued_books`
+--
+
+INSERT INTO `issued_books` (`s_no`, `book_no`, `book_name`, `book_author`, `student_id`, `status`, `issue_date`) VALUES
+(1, 6541, 'Data structure', 'D S Gupta', 1, 1, '2024-01-25'),
+(18, 7845, 'Programming in ANSI C', 'E. Balagurusamy', 0, 1, '2024-01-10'),
+(19, 111, 'A', 'Herbert Schildt', 111, 1, '24-02-19');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `issued_books_tbl`
+--
+
+CREATE TABLE `issued_books_tbl` (
+  `s_no` int(11) NOT NULL,
+  `book_no` int(11) NOT NULL,
+  `book_name` varchar(200) NOT NULL,
+  `book_author` varchar(200) NOT NULL,
+  `student_id` int(11) NOT NULL,
+  `status` int(11) NOT NULL,
+  `issue_date` longtext NOT NULL,
+  `issue_end_date` date NOT NULL,
+  `is_fine` int(11) NOT NULL,
+  `fine_amount` decimal(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(15) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `email` varchar(20) NOT NULL,
+  `password` varchar(30) NOT NULL,
+  `mobile` int(20) NOT NULL,
+  `address` varchar(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `mobile`, `address`) VALUES
+(1, 'youhana omi', 'omi@gmail.com', '12345', 1722222222, 'jashore ,sadar'),
+(2, 'Youhana Hakim ', 'Youhana@gmail.com', '123456', 144444444, 'dhaka'),
+(3, 'shaikh omi', 'shaikh@gmail.com', 'shaikh', 13456789, 'khulna'),
+(6, 'abcde', 'ab@gmail.com', '1234567', 1234567, 'khulna'),
+(7, 'abc', 'abc@gmail.com', '123', 147852, 'jashore');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admins`
+--
+ALTER TABLE `admins`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `authors`
+--
+ALTER TABLE `authors`
+  ADD PRIMARY KEY (`author_id`);
+
+--
+-- Indexes for table `books`
+--
+ALTER TABLE `books`
+  ADD PRIMARY KEY (`book_id`);
+
+--
+-- Indexes for table `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`cat_id`);
+
+--
+-- Indexes for table `issued_books`
+--
+ALTER TABLE `issued_books`
+  ADD PRIMARY KEY (`s_no`);
+
+--
+-- Indexes for table `issued_books_tbl`
+--
+ALTER TABLE `issued_books_tbl`
+  ADD PRIMARY KEY (`s_no`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admins`
+--
+ALTER TABLE `admins`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `authors`
+--
+ALTER TABLE `authors`
+  MODIFY `author_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=105;
+
+--
+-- AUTO_INCREMENT for table `books`
+--
+ALTER TABLE `books`
+  MODIFY `book_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `category`
+--
+ALTER TABLE `category`
+  MODIFY `cat_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT for table `issued_books`
+--
+ALTER TABLE `issued_books`
+  MODIFY `s_no` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+
+--
+-- AUTO_INCREMENT for table `issued_books_tbl`
+--
+ALTER TABLE `issued_books_tbl`
+  MODIFY `s_no` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
